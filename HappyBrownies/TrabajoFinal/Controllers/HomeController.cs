c60064ad6e15d56e8372731464ef60fd358b5da8
cam <cam@CAM-PC>
2017-07-06 01:08:16 -0500

    base de datos e interaccion

--- HappyBrownies/TrabajoFinal/Controllers/HomeController.cs
+++ HappyBrownies/TrabajoFinal/Controllers/HomeController.cs
@@ -8,10 +8,20 @@ namespace TrabajoFinal.Controllers
 {
     public class HomeController : Controller
     {
+
+        private Models.BDPastelEntities1 bd = new Models.BDPastelEntities1();
         // GET: Home
         public ActionResult Index()
         {
             return View();
         }
+        public ActionResult Buscar(string id="")
+        {
+            //lista de productos
+            var productos = bd.Producto.Where(x=>x.Descripcion.Contains(id)).Take(20).ToList();
+            ViewBag.clave = id;
+            return View(productos);
+        }
+
     }
 }
