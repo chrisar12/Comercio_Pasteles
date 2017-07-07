fea9c81105cad26d414a38fe7eeb8900fb1fffa3
cam <cam@CAM-PC>
2017-07-07 01:23:45 -0500

    menu admin

---
+++ HappyBrownies/TrabajoFinal/Areas/Admin/Controllers/paginaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrabajoFinal.Areas.Admin.Controllers
+{
+    public class paginaController : Controller
+    {
+        // GET: Admin/pagina
+        public ActionResult pagina()
+        {
+            return View();
+        }
+        public ActionResult inicio()
+        {
+            return View();
+        }
+    }
+}
